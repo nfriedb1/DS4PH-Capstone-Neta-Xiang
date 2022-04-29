a07d032bd259f3db0d9c2d03d55bfe983909985b
Neta Friedberg <nfriedb1@jhmi.edu>
2022-04-29 16:07:56 -0400

    map details

--- untitled.py
+++ untitled.py
@@ -1,8 +1,9 @@
 
 # geocodio test
+key = 3663c5c11ddc5f5b115452242dc6c6630c4c56c 	
 
 from geocodio import GeocodioClient
 
-client = GeocodioClient(3663c5c11ddc5f5b115452242dc6c6630c4c56c)
+client = GeocodioClient(key)
 
 addresses = client.reverse((38.9002898, -76.9990361))
